222b8851944804646a4dd41b6bd75f1b7b97eae4
eshan1925 <eshan1925@gmail.com>
2021-11-29 00:11:50 +0530

    Major Implementation Complete

--- src/viewRecordScreenController.java
+++ src/viewRecordScreenController.java
@@ -1,12 +1,16 @@
 import java.sql.*;
 import javax.swing.JOptionPane;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.util.Callback;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -21,26 +25,26 @@ public class viewRecordScreenController {
     @FXML
     private TextField studentname;
 
-    @FXML
-    private TableColumn<?, ?> highest;
+    // @FXML
+    // private TableColumn<?, ?> highest;
 
-    @FXML
-    private TableColumn<?, ?> percentage;
+    // @FXML
+    // private TableColumn<?, ?> percentage;
 
-    @FXML
-    private TableColumn<?, ?> sub1;
+    // @FXML
+    // private TableColumn<?, ?> sub1;
 
-    @FXML
-    private TableColumn<?, ?> sub2;
+    // @FXML
+    // private TableColumn<?, ?> sub2;
 
-    @FXML
-    private TableColumn<?, ?> sub3;
+    // @FXML
+    // private TableColumn<?, ?> sub3;
 
-    @FXML
-    private TableColumn<?, ?> sub4;
+    // @FXML
+    // private TableColumn<?, ?> sub4;
 
-    @FXML
-    private TableColumn<?, ?> sub5;
+    // @FXML
+    // private TableColumn<?, ?> sub5;
 
     @FXML
     private TableView<?> table;
@@ -57,30 +61,34 @@ public class viewRecordScreenController {
     void viewrecord(ActionEvent event) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db", "eshan", "eventour2020");
-        Statement s = con.createStatement();
         String a = '"' + studentname.getText() + '"';
         String s1 = "Select sub1,sub2,sub3,sub4,sub5,HighestMarks,Percentage from student_details where name=" + a;
-        ResultSet rsSet = s.executeQuery(s1);
-        ObservableList dbData = FXCollections.observableArrayList(rsSet);
-        for (int i = 0; i < rsSet.getMetaData().getColumnCount(); i++) {
-            TableColumn column = new TableColumn<>();
-            switch (rsSet.getMetaData().getColumnName(i+1)) {
-                case "id":
-                    column.setText("ID #");
-                    break;
-                case "name":
-                    column.setText("Person Name");
-                    break;
-                case "married":
-                    column.setText("Marital Status");
-                    break;
-                default: column.setText(rsSet.getMetaData().getColumnName(i+1)); //if column name in SQL Database is not found, then TableView column receive SQL Database current column name (not readable)
-                    break;
-            }
-            column.setCellValueFactory(new PropertyValueFactory<>(rsSet.getMetaData().getColumnName(i+1))); //Setting cell property value to correct variable from Person class.
-            table.getColumns().add(column);
+        ResultSet rsSet = con.createStatement().executeQuery(s1);
+        ObservableList data = FXCollections.observableArrayList();
+        for(int i=0 ; i<rsSet.getMetaData().getColumnCount(); i++){
+            //We are using non property style for making dynamic table
+            final int j = i;                
+            TableColumn col = new TableColumn(rsSet.getMetaData().getColumnName(i+1));
+            col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                    return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                }                    
+            });
+            table.getColumns().addAll(col); 
+            System.out.println("Column ["+i+"] ");
         }
-        table.setItems(dbData);
+
+        while(rsSet.next()){
+            //Iterate Row
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for(int i=1 ; i<=rsSet.getMetaData().getColumnCount(); i++){
+                //Iterate Column
+                row.add(rsSet.getString(i));
+            }
+            System.out.println("Row [1] added "+row );
+            data.add(row);
+        }
+        table.setItems(data);
     }
 
 }
